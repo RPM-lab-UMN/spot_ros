64d831ae78d1d4a4f2956567297ccb5575afcfae
ryan-roche <ry.roche@icloud.com>
2023-07-05 15:33:35 -0500

    Added new callbacks

--- spot_viz/scripts/interactive_marker_utils/__init__.py
+++ spot_viz/scripts/interactive_marker_utils/__init__.py
@@ -2,6 +2,8 @@ from .callbacks import (GoToMarkerCallback,
                         GoToRightMarkerCallback, 
                         GrabMarkerCallback, 
                         DragToMarkerCallback,
-                        TriggerCallback)
+                        TriggerCallback,
+                        MultiGraspToggleCallback,
+                        MultiGraspActionCallback)
 from .factory import create_interactive_marker
 from .pose_subscriber import MarkerPoseSubscriber
