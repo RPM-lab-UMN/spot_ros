3c1d6f4d4813a3606fd015a4dc5e2ef4e502c29a
AmitManicka <amitmanicka@gmail.com>
2023-07-10 10:43:41 -0500

    made fixes to recording_example.py

--- spot_driver/scripts/examples/recording_example.py
+++ spot_driver/scripts/examples/recording_example.py
@@ -12,7 +12,7 @@ from spot_driver.spot_wrapper import SpotWrapper
 
 import numpy as np
 class RecordingTester:
-    def __init__(self, uploadPath, power_off=False):
+    def __init__(self, power_off=False):
         '''
         NOTE: This script test the WorldObjectHandler class without requiring 
         any ros specific code. The wrapper could be used with other pure python 
@@ -36,38 +36,12 @@ class RecordingTester:
         self.spot.getLease(hijack=True)
         self.spot.power_on()
 
-
-
-
-
-    def __upload_recording_test__(self, uploadPath): #Test Script for testing an upload of a pre-recorded graph
-        self.log.debug('Attempting the upload Test...')
-
-        self.log.debug('Standing...')
-        self.spot.ensure_arm_power_and_stand()
-
-        self.log.debug('Attempting to clear any pre-uploaded maps...')
-        self.spot._clear_graph()
-
-        self.log.debug('Attempting to upload a pre-recorded map')
-        self.spot._upload_graph_and_snapshots(uploadPath)
-
-        self.log.debug('Attempting to list out the waypoint IDs')
-        self.spot._get_localization_state()
-        self.spot._list_graph_waypoint_and_edge_ids()
-
-        self.log.debug('Attempting to localize to waypoint number 1')
-        self.spot._set_initial_localization_waypoint(["cb"])
-
-        self.log.debug("Navigating waypoints...")
-
-        self.spot._navigate_to(["cb"])
-        time.sleep(4)
-        self.spot._navigate_route(["sd","db"])
-        time.sleep(2)
-        
-
-    def __obtain_recording_test__(self): #Test script for recording a path and downloading the recording to local machine
+    def obtain_recording_test(self):
+        '''
+        Function that tests downloading of a map
+        Commands the robot to stand up, walk one meter forward, and turn 90 degrees
+        Downloads the recording to local machine
+        '''
         self.log.debug('Standing...')
         self.spot.ensure_arm_power_and_stand()
 
@@ -95,42 +69,12 @@ class RecordingTester:
         self.log.debug('Attempting to download the recording')
         self.spot.download_recording()
 
-    #########################################################################################
-    ###### Ignore these #####################################################################
-    def __del__(self):
+    def __del__(self): #Destructor
         time.sleep(5)
         if self.power_off: self.spot.safe_power_off()
         self.spot.releaseLease()
         self.log.debug(f'Done')
 
-    def test_grasp(self):
-        self.log.debug('Testing Grasp.')
-        pose = np.eye(4); pose[0:3, 3] = [1.0, 0, 0]
-        self.task.grasp(pose, 'body')
-
-    def test_go_to(self):
-        pose = np.array([[ 0.0,  1.0,  0.0,  0.0],
-                        [-1.0,  0.0,  0.0,  0.0],
-                        [ 0.0,  0.0,  1.0,  0.0],
-                        [ 0.0,  0.0,  0.0,  1.0]])
-        self.task.go_to(pose, 'body', distance=0.0, dir_axis='x', up_axis='z')
-        # pose = np.eye(4); pose[0:3, 3] = [0.2, 0, 0]
-
-        self.log.debug('Turn 90 degrees counter-clockwise.')
-        pose = np.array([[ 0.0, -1.0,  0.0],
-                        [ 1.0,  0.0,  0.0],
-                        [ 0.0,  0.0,  1.0]])
-        self.task.go_to(pose, 'body', distance=0.0, dir_axis='x', up_axis='z')
-        
-        self.log.debug('Move forward 20cm.')
-        self.task.go_to(np.eye(4), 'body', distance=-0.2, dir_axis='x', up_axis='z')
-
-        self.log.debug('Move back 20cm.')
-        self.task.go_to(np.eye(4), 'body', distance=0.2, dir_axis='x', up_axis='z')
-
-
-if __name__=='__main__': # Here's where stuff is running
-    working_path = os.path.abspath("downloaded_graph")
-    print(working_path)
-    Testrun = RecordingTester(working_path)
-    Testrun.__upload_recording_test__(working_path)
+if __name__=='__main__':
+    Testrun = RecordingTester()
+    Testrun.obtain_recording_test()
