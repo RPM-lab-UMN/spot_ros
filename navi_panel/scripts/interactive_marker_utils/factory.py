0b87059d34e2e6ecf2ef190e3a760d37012e55eb
Ryan Roche <mnryanbr@gmail.com>
2023-08-07 14:25:24 -0500

    Added colors to factory

--- navi_panel/scripts/interactive_marker_utils/factory.py
+++ navi_panel/scripts/interactive_marker_utils/factory.py
@@ -9,10 +9,26 @@ from visualization_msgs.msg import *
 import math
 import rospy
 
+UNSELECTED_R = 0.45
+UNSELECTED_G = 0.06
+UNSELECTED_B = 0.67
 
-def create_waypoint_marker(name, reference_frame='odom', position=[1,1,1], orientation=[0,0,0,1]):
-    marker = Marker()
-    marker.type = Marker.CYLINDER
+
+def create_waypoint_marker(wp_id, reference_frame='odom', position=[1,1,1], orientation=[0,0,0,1]):
+    """
+    Creates an interactive, clickable marker for use in Rviz for representing graph navigation
+    waypoints for Hachi.
+
+    Args:
+        wp_id: The string of the waypoint ID. Used as both the marker name and description text.
+        reference_frame: The reference frame within which the marker is located
+        position: The initial position of the marker
+        orientation: The initial orientation of the marker
+    """
+
+    # Builds the marker in top-down order. The interactive marker message is the highest-level object,
+    # and is what ultimately is sent to the marker server for creation. It contains controls, which
+    # in-turn contain marker objects, which are the actual 3D shapes that you see in Rviz.
 
     int_marker_msg = InteractiveMarker()
     int_marker_msg.header.frame_id = reference_frame
@@ -20,8 +36,8 @@ def create_waypoint_marker(name, reference_frame='odom', position=[1,1,1], orien
     pos.x, pos.y, pos.z = tuple(position)
     ori = int_marker_msg.pose.orientation
     ori.x, ori.y, ori.z, ori.w = tuple(orientation)
-    int_marker_msg.name = name
-    int_marker_msg.description = "description"
+    int_marker_msg.name = "marker:" + wp_id
+    int_marker_msg.description = wp_id
 
     marker_control = InteractiveMarkerControl()
     marker_control.interaction_mode = InteractiveMarkerControl.BUTTON
@@ -29,5 +45,14 @@ def create_waypoint_marker(name, reference_frame='odom', position=[1,1,1], orien
     marker_control.name = "waypoint-click-button"
     marker_control.markers.append(marker)
     int_marker_msg.controls.append(marker_control)
+
+    marker = Marker()
+    marker.type = Marker.CYLINDER
+    marker.color.r = UNSELECTED_R
+    marker.color.g = UNSELECTED_G
+    marker.color.b = UNSELECTED_B
+    marker.scale.x = 0.5
+    marker.scale.y = 0.5
+    marker.scale.z = 1.0
 
     return int_marker_msg
